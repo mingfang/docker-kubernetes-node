0771474272f274938616f20c7a7ae3c16af9c1d5
root <root@ivy.home>
2015-02-05 18:16:49 -0500

    sets up openvswitch for docker automatically

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
 # Originally from https://gist.github.com/noteed/8656989#file-shared-docker-network-sh
+# This article was the most useful I found on the topic http://translate.google.com/translate?hl=en&sl=zh-CN&tl=en&u=http%3A%2F%2Faresy.blog.51cto.com%2F5100031%2F1600956
 
 echo "Make sure bridge-utils and openvswitch-switch are installed."
 echo "apt-get update && apt-get install -y bridge-utils openvswitch-switch"
 echo 
 
-# Edit this variable: the bridge address on 'this' host.
+# Find the last octet of the IP address
+HOST_IP=`ip route get 1 | awk '{print $NF;exit}'`
+echo "HOST_IP=$HOST_IP"
+LAST="${HOST_IP##*.}"
+echo "LAST=$LAST"
+
+# The subnet for all Docker containers on this host
 if [ -z "$BRIDGE_ADDRESS" ]
 then
-  BRIDGE_ADDRESS=10.244.1.1/16
+  BRIDGE_ADDRESS=10.244.$LAST.1/24
 fi
 echo "BRIDGE_ADDRESS=$BRIDGE_ADDRESS"
 
@@ -18,18 +27,21 @@ then
 fi
 echo "DOCKER_BRIDGE=$DOCKER_BRIDGE"
 
-# bridges
+# Docker bridge
 
 # Deactivate the DOCKER_BRIDGE bridge
 ip link set $DOCKER_BRIDGE down
 # Remove the DOCKER_BRIDGE bridge
 brctl delbr $DOCKER_BRIDGE
+
 # Add the DOCKER_BRIDGE bridge
 brctl addbr $DOCKER_BRIDGE
 # Set up the IP for the DOCKER_BRIDGE bridge
 ip a add $BRIDGE_ADDRESS dev $DOCKER_BRIDGE
 # Activate the bridge
 ip link set $DOCKER_BRIDGE up
+
+# OVS Bridge
 
 OVS_BRIDGE=obr0
 echo "OVS_BRIDGE=$OVS_BRIDGE"
@@ -43,8 +55,17 @@ ovs-vsctl set bridge $OVS_BRIDGE stp_enable=true
 # Add the OVS_BRIDGE bridge to DOCKER_BRIDGE bridge
 brctl addif $DOCKER_BRIDGE $OVS_BRIDGE
 
-# Edit this variable: the 'other' host.
-#REMOTE_IP=188.226.138.185
+# Create GRE
+ovs-vsctl add-port $OVS_BRIDGE tep0 -- set interface tep0 type=internal
+
+# Tunnel End Point, connects to the host and MUST have IP to work
+ip addr add 192.168.11.$LAST/24 dev tep0
+ip link set dev tep0 up
+
+# Enables routing to other hosts, critical
+ip route add 10.244.0.0/16 dev tep0 
+
+# Add Remote End Point for GRE, need one for each host, will use etcd for this
 if [ -z "$REMOTE_IP" ]
 then
   echo "REMOTE_IP is not set. Will not create tunnel."
@@ -55,23 +76,19 @@ else
   ovs-vsctl add-port $OVS_BRIDGE gre0 -- set interface gre0 type=gre options:remote_ip=$REMOTE_IP
 fi
 
-
+#don't know what to do with iptable yet
 # iptables rules
 
 # Enable NAT
-iptables -t nat -A POSTROUTING -s 10.244.0.0/16 ! -d 10.244.0.0/16 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -s 10.244.0.0/16 ! -d 10.244.0.0/16 -j MASQUERADE
 # Accept incoming packets for existing connections
-iptables -A FORWARD -o $DOCKER_BRIDGE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -o $DOCKER_BRIDGE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 # Accept all non-intercontainer outgoing packets
-iptables -A FORWARD -i $DOCKER_BRIDGE ! -o $DOCKER_BRIDGE -j ACCEPT
+#iptables -A FORWARD -i $DOCKER_BRIDGE ! -o $DOCKER_BRIDGE -j ACCEPT
 # By default allow all outgoing traffic
-iptables -A FORWARD -i $DOCKER_BRIDGE -o $DOCKER_BRIDGE -j ACCEPT
+#iptables -A FORWARD -i $DOCKER_BRIDGE -o $DOCKER_BRIDGE -j ACCEPT
 
-# Show bridges
-CMD="ip r s" && echo -e "\n$CMD" && eval $CMD
-CMD="route -n" && echo -e "\n$CMD" && eval $CMD
-CMD="brctl show" && echo -e "\n$CMD" && eval $CMD
-CMD="ovs-vsctl show" && echo -e "\n$CMD" && eval $CMD
+./ovs-show.sh
 
 # Restart Docker daemon to use the new DOCKER_BRIDGE
 echo -e "\nUse the OVS bridge by setting DOCKER_OPTS=\"--bridge=$DOCKER_BRIDGE\" in file /etc/defaults/docker."
