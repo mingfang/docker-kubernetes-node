6a9d7073c659278acbacae3489b2291ea94b4aed
root <root@ivy.home>
2015-02-08 00:05:12 -0500

    label hostname, correct memory

--- register.sh
+++ register.sh
@@ -4,13 +4,16 @@ HOST_IP=`ip route get 1 | awk '{print $NF;exit}'`
 cat <<END | kubectl create -f -
 {
   "id": "$HOST_IP",
-  "kind": "Minion",
+  "kind": "Node",
   "apiVersion": "v1beta2",
   "resources": {
     "capacity": {
       "cpu": "1000",
-      "memory": 1073741824
+      "memory": `free -b|grep Mem|awk '{print $2}'` 
     }
+  },
+  "labels": {
+    "hostname":"`hostname`"
   }
 }
 END
