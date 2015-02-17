4b0062da1428762509647f119469e1786d733b08
root <root@vm1.home>
2015-02-17 04:12:49 +0000

    fixed string comparison

--- ovs-remote.sh
+++ ovs-remote.sh
@@ -14,16 +14,16 @@ else
   GRE="gre${REMOTE_IP##*.}"
   echo "GRE=$GRE" 
 
-  if [ "add" -eq "$1"]
+  if [ "add" = "$1" ]
   then
     # Create the tunnel to the other host and attach it to the OVS_BRIDGE bridge
-    ovs-vsctl add-port $OVS_BRIDGE $GRE -- set interface $GRE type=gre options:remote_ip=$REMOTE_IP
+    CMD="ovs-vsctl add-port $OVS_BRIDGE $GRE -- set interface $GRE type=gre options:remote_ip=$REMOTE_IP" && echo -e "\n$CMD" && eval $CMD
   fi
 
-  if [ "del" -eq "$1" ]
+  if [ "del" = "$1" ]
   then
     # Delete port
-    ovs-vsctl del-port $OVS_BRIDGE $GRE
+    CMD="ovs-vsctl del-port $OVS_BRIDGE $GRE" && echo -e "\n$CMD" && eval $CMD
   fi
 
   CMD="ovs-vsctl show" && echo -e "\n$CMD" && eval $CMD
