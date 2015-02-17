4b7b1a0c9f1caf4c49c8f64a0ef356674497e31a
root <root@vm2.home>
2015-02-17 04:06:28 +0000

    sync ovs tunnels with etcd

--- ovs-remote.sh
+++ ovs-remote.sh
@@ -3,10 +3,10 @@
 OVS_BRIDGE=obr0
 echo "OVS_BRIDGE=$OVS_BRIDGE"
 
-REMOTE_IP=$1
+REMOTE_IP=$2
 if [ -z "$REMOTE_IP" ]
 then
-  echo "REMOTE_IP is not set. Will not create tunnel."
+  echo "REMOTE_IP is not set."
 else
   echo "REMOTE_IP=$REMOTE_IP"
 
@@ -14,8 +14,17 @@ else
   GRE="gre${REMOTE_IP##*.}"
   echo "GRE=$GRE" 
 
-  # Create the tunnel to the other host and attach it to the OVS_BRIDGE bridge
-  ovs-vsctl add-port $OVS_BRIDGE $GRE -- set interface $GRE type=gre options:remote_ip=$REMOTE_IP
+  if [ "add" -eq "$1"]
+  then
+    # Create the tunnel to the other host and attach it to the OVS_BRIDGE bridge
+    ovs-vsctl add-port $OVS_BRIDGE $GRE -- set interface $GRE type=gre options:remote_ip=$REMOTE_IP
+  fi
+
+  if [ "del" -eq "$1" ]
+  then
+    # Delete port
+    ovs-vsctl del-port $OVS_BRIDGE $GRE
+  fi
 
   CMD="ovs-vsctl show" && echo -e "\n$CMD" && eval $CMD
 fi
