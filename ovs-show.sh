64bb5aedfdf891498e50e86f1e6e213fe30be185
root <root@vm2.home>
2015-09-21 04:01:43 +0000

    Revert "use openflow"
    This reverts commit 6bf639234912578d27414d4e6a5db2bade4648d6.

--- ovs-show.sh
+++ ovs-show.sh
@@ -4,6 +4,4 @@ CMD="ip r s" && echo -e "\n$CMD" && eval $CMD
 CMD="route -n" && echo -e "\n$CMD" && eval $CMD
 CMD="brctl show" && echo -e "\n$CMD" && eval $CMD
 CMD="ovs-vsctl show" && echo -e "\n$CMD" && eval $CMD
-CMD="ovs-ofctl -O OpenFlow13 dump-ports-desc obr0" && echo -e "\n$CMD" && eval $CMD
-CMD="ovs-ofctl -O OpenFlow13 dump-flows obr0" && echo -e "\n$CMD" && eval $CMD
 
