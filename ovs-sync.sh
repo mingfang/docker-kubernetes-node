76cfb06f5e045fc0cfe8813fb0ee980ad22a1ab0
Ming Fang <mingfang@mac.com>
2016-05-30 14:14:24 -0400

    handle error when master is down

--- ovs-sync.sh
+++ ovs-sync.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+set -e
+
 LOCAL=`ovs-vsctl list interface|grep options|grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|sort`
-REMOTE=`curl -s -L $ETCD_HOST:4001/v2/keys/registry/minions|jq .node.nodes[].key | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|sort`
+CURL=`curl --fail -s -L $ETCD_HOST:4001/v2/keys/registry/minions`
+REMOTE=`echo $CURL|jq .node.nodes[].key | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|sort`
 
 echo "LOCAL=$LOCAL"
 echo "REMOTE=$REMOTE"
