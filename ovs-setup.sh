60339051bedc88217546250842a2f208f9a7a3ba
root <root@minux.home>
2016-07-02 06:12:18 -0400

    removed tep0

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -12,7 +12,7 @@ HOST_IP=`ip route get 1 | awk '{print $NF;exit}'`
 LAST="${HOST_IP##*.}"
 
 # The subnet for all Docker containers on this host
-BRIDGE_ADDRESS=10.244.$LAST.1/24
+BRIDGE_ADDRESS=10.244.$LAST.1
 
 # Name of the bridge (should match /etc/default/docker).
 DOCKER_BRIDGE=kbr0
@@ -24,13 +24,10 @@ echo "DOCKER_BRIDGE=$DOCKER_BRIDGE"
 
 # Docker bridge
 
-# Deactivate the DOCKER_BRIDGE bridge
 ip link set $DOCKER_BRIDGE down
 brctl delbr $DOCKER_BRIDGE
-
-# Add the DOCKER_BRIDGE bridge
 brctl addbr $DOCKER_BRIDGE
-ip a add $BRIDGE_ADDRESS dev $DOCKER_BRIDGE
+ip addr add ${BRIDGE_ADDRESS}/24 dev $DOCKER_BRIDGE
 ip link set $DOCKER_BRIDGE up
 
 # OVS Bridge
@@ -38,26 +35,14 @@ ip link set $DOCKER_BRIDGE up
 OVS_BRIDGE=obr0
 echo "OVS_BRIDGE=$OVS_BRIDGE"
 
-# Delete the Open vSwitch bridge
 ovs-vsctl del-br $OVS_BRIDGE
-# Add the OVS_BRIDGE Open vSwitch bridge
 ovs-vsctl add-br $OVS_BRIDGE
-# Enable STP
 ovs-vsctl set bridge $OVS_BRIDGE stp_enable=true
-# Add the OVS_BRIDGE bridge to DOCKER_BRIDGE bridge
 brctl addif $DOCKER_BRIDGE $OVS_BRIDGE
-#enable OVS_BRIDGE
 ip link set $OVS_BRIDGE up
 
-# Create GRE
-ovs-vsctl add-port $OVS_BRIDGE tep0 -- set interface tep0 type=internal
-
-# Tunnel End Point, connects to the host and MUST have IP to work
-ip addr add 192.168.11.$LAST/24 dev tep0
-ip link set dev tep0 up
-
 # Enables routing to other hosts, critical
-ip route add 10.244.0.0/16 dev tep0 
+ip route add 10.244.0.0/16 dev $DOCKER_BRIDGE scope link src $BRIDGE_ADDRESS
 
 ./ovs-show.sh
 
