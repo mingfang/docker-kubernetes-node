6da0b2eda3392b51b43f35c1b9b595ec444b8250
root <root@ivy.home>
2017-01-10 01:00:43 -0500

    cleanup fan

--- fan-setup.sh
+++ fan-setup.sh
@@ -10,7 +10,6 @@ UNDERLAY="${PRIMARY}/16"
 OVERLAY="250.0.0.0/8"
 IFS=. read ip1 ip2 ip3 ip4 <<< "$PRIMARY"
 DOCKER_CIDR="250.$ip3.$ip4.0/24"
-DOCKER_BIP="250.$ip3.$ip4.1/24"
 
 # Name of the bridge (should match /etc/default/docker).
 DOCKER_BRIDGE=kbr0
@@ -25,12 +24,14 @@ echo "DOCKER_CIDR=$DOCKER_CIDR"
 fanctl down -e
 fanctl up -u $UNDERLAY -o $OVERLAY --bridge=$DOCKER_BRIDGE
 fanctl show
+rm -r /var/lib/docker/network/files/local-kv.db
 
 # Restart Docker daemon to use the new DOCKER_BRIDGE
-DOCKER_OPTS="--bip=$DOCKER_BIP --mtu=1450 --iptables=false --insecure-registry=0.0.0.0/0"
-echo -e "\nUse the Fan bridge by setting DOCKER_OPTS=\"$DOCKER_OPTS\" in file /etc/default/docker."
+DOCKER_OPTS="--bridge=kbr0 --fixed-cidr=$DOCKER_CIDR --mtu=1450 --iptables=false --insecure-registry=0.0.0.0/0"
+echo -e "\nUse the Fan bridge by setting DOCKER_OPTS=\"$DOCKER_OPTS\" in file /etc/default/docker"
 echo "
-#/etc/systemd/system/docker.service
+Ubuntu 16.04+
+#/etc/systemd/system/docker.service.d/docker.conf
 [Service]
 ExecStart=
 ExecStart=/usr/bin/dockerd $DOCKER_OPTS --storage-driver=zfs
