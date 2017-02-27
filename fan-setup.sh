63480afc02e118940fe8b4099c681e8da5eedd7c
root <root@minux.home>
2017-02-26 23:27:02 -0500

    auto detech zfs

--- fan-setup.sh
+++ fan-setup.sh
@@ -13,15 +13,7 @@ UNDERLAY="${PRIMARY}/16"
 OVERLAY="250.0.0.0/8"
 IFS=. read ip1 ip2 ip3 ip4 <<< "$PRIMARY"
 DOCKER_CIDR="250.$ip3.$ip4.0/24"
-
-# Name of the bridge (should match /etc/default/docker).
 DOCKER_BRIDGE=kbr0
-
-echo "PRIMARY=$PRIMARY"
-echo "UNDERLAY=$UNDERLAY"
-echo "OVERLAY=$OVERLAY"
-echo "DOCKER_BRIDGE=$DOCKER_BRIDGE"
-echo "DOCKER_CIDR=$DOCKER_CIDR"
 
 # Fan bridge
 fanctl down -e
@@ -30,7 +22,8 @@ fanctl show
 rm -r /var/lib/docker/network/files/local-kv.db
 
 # Restart Docker daemon to use the new DOCKER_BRIDGE
-DOCKER_OPTS="--bridge=kbr0 --fixed-cidr=$DOCKER_CIDR --mtu=1450 --iptables=true --insecure-registry=0.0.0.0/0 --storage-driver=zfs"
+ZFS=$([ `df --output=fstype /var/lib/docker|tail -1` == "zfs" ] && echo "--storage-driver=zfs" || echo  "")
+DOCKER_OPTS="--bridge=kbr0 --fixed-cidr=$DOCKER_CIDR --mtu=1450 --iptables=true --insecure-registry=0.0.0.0/0 $ZFS"
 
 #/etc/systemd/system/docker.service.d/docker.conf
 mkdir -p /etc/systemd/system/docker.service.d
@@ -38,5 +31,11 @@ printf "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd $DOCKER_OPTS" > /etc/s
 
 echo "Restarting Docker..."
 systemctl daemon-reload
-systemctl restart docker
-docker info
+systemctl restart docker --ignore-dependencies
+
+echo "PRIMARY=$PRIMARY"
+echo "UNDERLAY=$UNDERLAY"
+echo "OVERLAY=$OVERLAY"
+echo "DOCKER_CIDR=$DOCKER_CIDR"
+echo "DOCKER_BRIDGE=$DOCKER_BRIDGE"
+echo "DOCKER_OPTS=$DOCKER_OPTS"
